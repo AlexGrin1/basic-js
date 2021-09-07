cec1b7978b6051e95ee69b6ce06a0106f1779709
AlexGrin1 <75309659+AlexGrin1@users.noreply.github.com>
2021-09-07 23:23:05 +0300

    feat: solved task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,23 +5,37 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default {
-  getLength() {
+  maker:[],
+  getLength() {return this.maker.length
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   },
-  addLink(/* value */) {
+  addLink(value) {
+		this.maker.push(`(${value})`)
+		return this
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   },
-  removeLink(/* position */) {
+  removeLink(value) {
+		if(typeof value !=="number" ||!Number.isInteger(value) || value===undefined){
+throw new Error ("You can't remove incorrect link!")			
+		}
+		else{
+			this.maker.splice(value-1,1);
+		return this	
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   },
   reverseChain() {
+    this.maker.reverse()
+		return this
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   },
   finishChain() {
+    let result=[...this.maker]
+		this.maker.length=0
+		return result.join('~~')}
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
